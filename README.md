4909f0594688014dd77e4f136ab9167e79e6c5bb
Nir Gazit <nirga@users.noreply.github.com>
2024-07-05 12:31:13 +0200

    feat: basic chat engine over docs

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# demo
-A full demo of OpenLLMetry with LlamaIndex on Traceloop docs. See traceloop.com/demo
+## A full demo of Traceloop observability with LlamaIndex on our docs
+
+See https://traceloop.com/demo
+
+1. Install:
+
+```python
+poetry install
+```
+
+2. Set up the environment:
+
+```python
+export GITHUB_TOKEN=<your github token> # Needed to index our docs from github.com/traceloop/docs
+export OPENAI_API_KEY=<your openai api key>
+export TRACELOOP_API_KEY=<your traceloop api key>
+```
+
+You can connect OpenLLMetry to other platform by following one of the guides [here](https://www.traceloop.com/docs/openllmetry/integrations/introduction).
+
+3. Run the demo locally like this:
+
+```python
+poetry run streamlit run demo/app.py
+```
---
+++ demo/app.py
@@ -0,0 +1,79 @@
+import os
+import streamlit as st
+from llama_index.core import VectorStoreIndex
+from llama_index.core import Settings
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.readers.github import GithubRepositoryReader, GithubClient
+
+from traceloop.sdk import Traceloop
+
+Traceloop.init()
+
+Settings.llm = OpenAI(model="gpt-4o")
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+
+github_client = GithubClient(github_token=os.environ["GITHUB_TOKEN"])
+
+st.set_page_config(
+    page_title="Traceloop Demo",
+    page_icon="🔭",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+st.title("Traceloop Demo")
+st.info(
+    "Do not share any private information here. "
+    + "All traces can be viewed by anyone who signs up at https://app.traceloop.com"
+)
+
+if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "Ask me a question about Traceloop or OpenLLMetry!",
+        }
+    ]
+
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing Traceloop and OpenLLMetry docs."):
+        reader = GithubRepositoryReader(
+            github_client=github_client,
+            owner="traceloop",
+            repo="docs",
+            filter_file_extensions=(
+                [".mdx"],
+                GithubRepositoryReader.FilterType.INCLUDE,
+            ),
+            verbose=True,
+        )
+        docs = reader.load_data(branch="main")
+        index = VectorStoreIndex.from_documents(docs)
+        return index
+
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="best", verbose=True)
+
+if prompt := st.chat_input(
+    "Your question"
+):  # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages:  # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message)  # Add response to message history
